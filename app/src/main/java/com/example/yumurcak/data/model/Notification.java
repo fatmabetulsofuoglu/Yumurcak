80c4daf0f892edeac2a7b5f32fd37f97a5df3860
betulturkut <40726607+betulturkut@users.noreply.github.com>
2019-09-29 14:21:02 +0300

    Notification.java filled

--- app/src/main/java/com/example/yumurcak/data/model/Notification.java
+++ app/src/main/java/com/example/yumurcak/data/model/Notification.java
@@ -1,4 +1,37 @@
 package com.example.yumurcak.data.model;
 
 public class Notification {
+    private String userPhotoURL;
+    private String userName;
+    private String notificationsContext; //Bildirim text kısmı.
+
+    public Notification(String userPhotoURL, String userName, String notificationsContext) {
+        this.userPhotoURL = userPhotoURL;
+        this.userName = userName;
+        this.notificationsContext = notificationsContext;
+    }
+
+    public String getUserPhotoURL() {
+        return userPhotoURL;
+    }
+
+    public void setUserPhotoURL(String userPhotoURL) {
+        this.userPhotoURL = userPhotoURL;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getNotificationsContext() {
+        return notificationsContext;
+    }
+
+    public void setNotificationsContext(String notificationsContext) {
+        this.notificationsContext = notificationsContext;
+    }
 }
